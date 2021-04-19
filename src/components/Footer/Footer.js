23ea4b57445de080e7346bae45bc0fa4852f9da2
Nikolina Babok <nbabok@foi.hr>
2021-04-19 10:58:24 +0200

    First commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.scss';
+
+const Footer = () => {
+    return ( 
+        <footer class="Footer">
+            <div>
+                <p class= "Footer-Text">Copyright &copy; 2021 Speck Academy</p>
+            </div>
+        </footer>
+     );
+}
+ 
+export default Footer;
